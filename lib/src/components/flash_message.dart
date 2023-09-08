1c3b1f1f192d74559283bc3d1a5fc5965de694a4
Rockefeller <rockefel@kenda.com.tw>
2023-09-08 14:13:08 +0800

    refactor: using a future builder instead of a stateful widget

--- lib/src/components/flash_message.dart
+++ lib/src/components/flash_message.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
-  void showFlashMessage(BuildContext context, String message) {
-    final snackBar = SnackBar(
-      content: Text(message),
-      duration: const Duration(seconds: 2),
-    );
+void showFlashMessage(BuildContext context, String message) {
+  final snackBar = SnackBar(
+    content: Text(message),
+    duration: const Duration(seconds: 2),
+  );
 
-    ScaffoldMessenger.of(context).showSnackBar(snackBar);
-  }
+  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+}
--- lib/src/features/graph_view/graph_view.dart
+++ lib/src/features/graph_view/graph_view.dart
@@ -4,32 +4,30 @@ import 'package:langdida_ui/src/components/app_bar.dart';
 import 'package:langdida_ui/src/components/flash_message.dart';
 import 'package:langdida_ui/src/features/graph_view/animation.dart';
 import 'package:langdida_ui/src/utils/connections.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
-class GraphViewPage extends StatefulWidget {
+class GraphViewPage extends StatelessWidget {
   const GraphViewPage({super.key});
 
-  @override
-  State<StatefulWidget> createState() => _GraphViewPageState();
-}
-
-class _GraphViewPageState extends State<GraphViewPage> {
-  List<CardIndex> indexes = [];
-  Widget _bodyWidget = const Text("Loading...");
-  _GraphViewPageState() {
-    Connections.listCardIndexes().then((value) {
-      indexes = value;
-      setState(() {
-        _bodyWidget = AnimatedCanvas(cards: indexes);
-      });
-    }).catchError((error) {
-      showFlashMessage(context, error.toString());
-    });
-  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: newLangDiDaAppBar("relationships", context),
-      body: _bodyWidget,
+      body: FutureBuilder(
+        future: Connections.listCardIndexes(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState != ConnectionState.done) {
+            return LoadingAnimationWidget.discreteCircle(
+                color: Colors.blue, size: 20);
+          }
+          if (snapshot.hasError || snapshot.data == null) {
+            showFlashMessage(context, "Error on loading card indices");
+            return LoadingAnimationWidget.discreteCircle(
+                color: Colors.blue, size: 20);
+          }
+          return AnimatedCanvas(cards: snapshot.data!);
+        },
+      ),
     );
   }
 }
